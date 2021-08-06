bc30777aab2e3f66a4954fd0949517100761ffb2
Swaraj routray <swarajroutray961@gmail.com>
2021-08-06 17:03:02 +0530

    fixed footer

--- src/components/presentation/footer.js
+++ src/components/presentation/footer.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 const footer = () => {
+  const iconstyle = {
+    paddingTop:"2rem",
+    
+  };
   return (  
   
-  <footer className="footer">  
-    <div>
-     
-    </div>
-  </footer>
+    <footer className="footer">
+       
+       <div className="icons" style={iconstyle}>
+          <center>
+          <a href="#"><i class="fab fa-facebook"></i ></a>
+           <a href="#"><i class="fab fa-linkedin"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+          </center>
+          <center><p class="company-name">
+                Swaraj Routray &copy; 2021, ALL Rights Reserved
+            </p></center>
+        </div>
+    </footer>
 
   );
 };
